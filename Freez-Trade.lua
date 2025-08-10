40ce79210e75a82002d847afd8f5f5b24100c8b1
Biggg886 <154685671+Biggg886@users.noreply.github.com>
2025-08-10 15:52:52 +0700

    Update Freez-Trade.lua

--- Freez-Trade.lua
+++ Freez-Trade.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Biggg886/Dkkdk/refs/heads/main/Sjdk.lua"))()
-loadstring(game:HttpGet("https://pastefy.app/zpilRNnk/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/2wJfZd1V/raw"))()
